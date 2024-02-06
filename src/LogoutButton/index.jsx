1ba96b9c9739114815afd2d31157462c529e3631
Paul Ivey <paivey@microsoft.com>
2024-02-06 23:49:51 +0000

    Fixed the logout button avatar

--- src/LogoutButton/index.jsx
+++ src/LogoutButton/index.jsx
@@ -1,5 +1,5 @@
-import { useMsal } from "@azure/msal-react";
-import { useIsAuthenticated } from "@azure/msal-react";
+import { useMsal, useIsAuthenticated } from "@azure/msal-react";
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import { Button } from '@fluentui/react-components';
 import { Image } from '@fluentui/react-components';
@@ -9,6 +9,18 @@ export default function LogoutButton() {
   const isAuthenticated = useIsAuthenticated();
   const navigate = useNavigate();
   const { instance: msalInstance } = useMsal();
+  const [avatarUrl, setAvatarUrl] = useState(null);
+
+  useEffect(() => {
+    const fetchAvatar = async () => {
+      const response = await getUserAvatar(msalInstance);
+      const blob = await response.blob();
+      const avatarUrl = URL.createObjectURL(blob);
+      setAvatarUrl(avatarUrl);
+    };
+
+    fetchAvatar();
+  }, [msalInstance]);
 
   async function handleLogout(event) {
     event.preventDefault();
@@ -21,7 +33,7 @@ export default function LogoutButton() {
   return (
     <Button size="small" appearance="subtle" style={{marginLeft:'0.5rem'}} onClick={handleLogout}>
       <Image
-        src="https://media.licdn.com/dms/image/D4E03AQEpuhuYeJ29Ow/profile-displayphoto-shrink_200_200/0/1676289501661?e=1712793600&v=beta&t=jPARjYKWMJ8pcqu-CdMwsTGgnFD4xzOcYBwaynauZxc"
+        src={avatarUrl}
         alt="User avatar"
         style={{width:'1rem', height:'1rem', borderRadius:'50%', marginRight: '2px'}}
       />
